ec8260c8e51dbb166612eae3ca68be6804e4ea9d
1F10240151 <s1f102401517@iniad.org>
2025-07-10 11:29:31 +0900

    更新機能の追加

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from todo import views as todo_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", todo_views.index, name="index"),
-    path("<int:task_id>/", todo_views.detail, name="detail"),
+    path('', todo_views.index, name='index'),
+    path('<int:task_id>/', todo_views.detail, name='detail'),
+    path('<int:task_id>/update', todo_views.update, name='update'),
 ]
